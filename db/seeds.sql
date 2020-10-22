ce576fb43d4ae30309a494d00d74d5b08712683a
Leslie Villatoro <levill98@gmail.com>
2020-10-22 15:11:28 -0700

    added db folder with seeds and schema sql

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Veggie burger");
+
+INSERT INTO burgers (burger_name) VALUES ("In n Out burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Big Mac");
